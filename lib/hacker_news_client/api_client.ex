d2f2fdf6a23b5ce3ffce74a4960b7657dce08087
Leandro Nishijima <4566637+leandronishijima@users.noreply.github.com>
2022-02-08 13:37:23 -0300

    add query params to limit the amount of stories

--- lib/hacker_news_client/api_client.ex
+++ lib/hacker_news_client/api_client.ex
@@ -2,12 +2,14 @@ defmodule HackerNewsAggregator.HackerNewsClient.ApiClient do
   alias Finch.Response
 
   @base_url "https://hacker-news.firebaseio.com/v0"
+  @params_to_limit_stories "print=pretty&limitToFirst=50&orderBy=%22$key%22"
 
   def child_spec(_) do
     {Finch, name: __MODULE__}
   end
 
-  def top_stories, do: get("#{@base_url}/topstories.json")
+  def top_stories,
+    do: get("#{@base_url}/topstories.json?#{@params_to_limit_stories}")
 
   def item(item_id), do: get("#{@base_url}/item/#{item_id}.json")
 
