f3a60abc3de6187ba53b0b20cf18d46dd79f6f03
Leandro Nishijima <4566637+leandronishijima@users.noreply.github.com>
2022-02-10 21:13:19 -0300

    refactorings

--- lib/hacker_news_client/api_client.ex
+++ lib/hacker_news_client/api_client.ex
@@ -2,14 +2,14 @@ defmodule HackerNewsAggregator.HackerNewsClient.ApiClient do
   alias Finch.Response
 
   @base_url "https://hacker-news.firebaseio.com/v0"
-  @params_to_limit_stories "print=pretty&limitToFirst=50&orderBy=%22$key%22"
+  @params_to_limit_stories_by_50 "print=pretty&limitToFirst=50&orderBy=%22$key%22"
 
   def child_spec(_) do
     {Finch, name: __MODULE__}
   end
 
   def top_stories,
-    do: get("#{@base_url}/topstories.json?#{@params_to_limit_stories}")
+    do: get("#{@base_url}/topstories.json?#{@params_to_limit_stories_by_50}")
 
   def item(item_id), do: get("#{@base_url}/item/#{item_id}.json")
 
@@ -20,7 +20,5 @@ defmodule HackerNewsAggregator.HackerNewsClient.ApiClient do
     |> parse_response()
   end
 
-  defp parse_response({:ok, %Response{body: body, status: 200}}) do
-    Jason.decode(body)
-  end
+  defp parse_response({:ok, %Response{body: body, status: 200}}), do: Jason.decode(body)
 end
