6f5e108d96a0c67dc07f2e8981c6f083d7673213
Leandro Nishijima <4566637+leandronishijima@users.noreply.github.com>
2022-02-19 10:40:13 -0300

    testing broadcast socket

--- lib/socket/fetch_top_stories_handler.ex
+++ lib/socket/fetch_top_stories_handler.ex
@@ -3,22 +3,35 @@ defmodule HackerNewsAggregator.Socket.FetchTopStoriesHandler do
 
   alias HackerNewsAggregator.Api
 
-  def init(req, _state) do
-    {:cowboy_websocket, req, %{query: req.qs}}
+  def init(req, state) do
+    {:cowboy_websocket, req, state}
   end
 
   def websocket_init(state) do
     {:ok, state}
   end
 
-  def websocket_handle({:text, _message}, %{query: query} = state) do
-    response_json = Api.get_paginate_top_stories(Api, query)
-    {:reply, {:text, response_json}, state}
+  def websocket_handle({:text, _message}, state) do
+    IO.puts("handle -> PID = #{inspect(self())}")
+    top_stories = Api.get_top_stories(Api)
+    {:reply, {:text, top_stories}, state}
   end
 
-  def websocket_info(_info, state) do
+  # broadcast handle here
+  def websocket_info(info, state) do
+    IO.inspect("called info = #{info} and state = #{state}")
     {:reply, state}
   end
+
+  def loop(req) do
+    receive do
+      {:broadcast, data} ->
+        IO.puts "received [ #{data} ]"
+        __MODULE__.websocket_info({:info, data}, :undefined_state)
+        loop(req)
+    end
+  end
+
 
   def terminate(_reason, _req, _state) do
     :ok
