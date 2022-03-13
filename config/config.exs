e28576e4f08f03b71fe51d94d2af7caa2c99213d
Leandro Nishijima <4566637+leandronishijima@users.noreply.github.com>
2022-03-13 09:07:03 -0300

    config files

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,7 @@
 import Config
 
 config :ex_hacker_news_aggregator,
-  hacker_news_api: HackerNewsAggregator.HackerNews.ApiImpl
+  hacker_news_api: HackerNewsAggregator.HackerNews.ApiImpl,
+  fetch_interval_time_in_seconds: 300
 
 import_config "#{config_env()}.exs"
--- config/dev.exs
+++ config/dev.exs
@@ -1,1 +1,4 @@
 import Config
+
+config :ex_hacker_news_aggregator,
+  fetch_interval_time_in_seconds: 30
